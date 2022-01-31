566a83fcf5ea4f5f7ad741ba54a8f83f38b75b3f
Kimberley Ni <wukimberley98@gmail.com>
2022-01-31 16:30:54 -0500

    Finished Battle Screen

--- screen_battle.py
+++ screen_battle.py
@@ -26,22 +26,21 @@ class Screen_Battle (tk.Frame):
         '''
         self.atk_bttn = tk.Button(self, text = "Attack!", bg = 'black', fg = 'red')
         self.atk_bttn["command"] = self.attack_clicked
-        self.atk_bttn.grid(row = 0, column = 0, pady = 5, sticky = tk.N)
+        self.atk_bttn.grid(row = 0, column = 0)
 
         # three lines of labels for move announcements
         self.annc_atk = tk.Label(self) 
-        self.annc_atk.grid(row = 0, column = 2, sticky = tk.N) 
+        self.annc_atk.grid(row = 0, column = 1, sticky = tk.N) 
         self.annc_atk2 = tk.Label(self)
-        self.annc_atk.grid(row = 1, column = 2, sticky = tk.N) 
+        self.annc_atk2.grid(row = 1, column = 1, sticky = tk.N)
         self.annc_atk3 = tk.Label(self) 
-        self.annc_atk3.grid(row = 2, column = 2, sticky = tk.N) 
+        self.annc_atk3.grid(row = 2, column = 1, sticky = tk.N) 
 
         self.player_lbl = tk.Label(self, text = "You", font=("Times New Roman", 14))
         self.player_lbl.grid(row = 3, column = 0, sticky = tk.W + tk.E)
         
         self.computer_lbl = tk.Label(self, text = "Computer", font=("Times New Roman", 14), fg = 'red')
         self.computer_lbl.grid(row = 3, column = 1, sticky = tk.W + tk.E)
-
 
         player_img = tk.PhotoImage(file="images/" + self.player1.large_image)
         p1 = tk.Label(self, image = player_img)
@@ -53,48 +52,34 @@ class Screen_Battle (tk.Frame):
         p2.photo = computer_img
         p2.grid(row = 4, column = 1, sticky = tk.N)
 
-        self.init_player_hp = self.player1.hit_points
-        self.init_computer_hp = self.player2.hit_points
-
-        # finish adjusting hp labels to match example
-        self.player_hp_lbl = tk.Label(self, text = f"{self.player1.hit_points}/{self.init_player_hp} HP")
+        # HP labels
+        self.player_hp_lbl = tk.Label(self, text = f"{self.player1.hit_points}/{self.player1_max_hp} HP")
         self.player_hp_lbl.grid(row = 5, column = 0, sticky = tk.N)
 
-        self.computer_hp_lbl = tk.Label(self, text = f"{self.player2.hit_points}/{self.init_computer_hp} HP")
+        self.computer_hp_lbl = tk.Label(self, text = f"{self.player2.hit_points}/{self.player2_max_hp} HP")
         self.computer_hp_lbl.grid(row = 5, column = 1, sticky = tk.N)
 
-        self.commence_bttn = tk.Button(self, text = "Exit", bg = 'black', fg = 'red')
-        self.commence_bttn["command"] = self.exit_clicked
-
-        
+        self.exit_bttn = tk.Button(self, text = "Exit", bg = 'black', fg = 'red')
+        self.exit_bttn["command"] = self.exit_clicked
         
     def attack_clicked(self):
-        self.annc_atk['text'] = self.player1.attack(self.player2)
+        self.annc_atk['text'] = str(self.player1.attack(self.player2))
         
-        if self.player2.hit_points > 0:
+        if self.player2.hit_points > 0: 
             self.annc_atk2['text'] = self.player2.attack(self.player1)
-
-        if self.player1.hit_points <= 0: 
-            self.annc_atk3(self, text = self.player2.name + "wins")
+        else: # computer dead
+            self.annc_atk3['text'] = self.player1.name + " wins!"
             self.atk_bttn.destroy() 
-            self.exit_btn = tk.Button(self, text = "Exit", bg = 'black', fg = 'red')
-            self.exit_btn["command"] = self.exit_clicked()
-            self.exit_btn.grid(row = 0, column = 0, pady = 5, sticky = tk.N)
+            self.exit_bttn.grid(row = 6, column = 1, pady = 5, sticky = tk.S + tk.E)
 
-        elif self.player2.hit_points <= 0: 
-            self.annc_atk3(self, text = self.player1.name + "wins")
+        if self.player1.hit_points <= 0: # player dead
+            self.annc_atk3['text'] = self.player2.name + " wins!"
             self.atk_bttn.destroy() 
-            self.exit_btn = tk.Button(self, text = "Exit", bg = 'black', fg = 'red')
-            self.exit_btn["command"] = self.exit_clicked()
-            self.exit_btn.grid(row = 0, column = 0, pady = 5, sticky = tk.N)
+            self.exit_bttn.grid(row = 6, column = 1, pady = 5, sticky = tk.S + tk.E)
 
-        self.player_hp_lbl = tk.Label(self)
+        # update HP labels
         self.player_hp_lbl["text"] = f"{self.player1.hit_points}/{self.player1_max_hp} HP"
-        self.player_hp_lbl.grid(row = 3, column = 0, sticky = tk.N)
-
-        self.computer_hp_lbl = tk.Label(self)
         self.computer_hp_lbl["text"] = f"{self.player2.hit_points}/{self.player2_max_hp} HP"
-        self.computer_hp_lbl.grid(row = 3, column = 1, sticky = tk.N)
 
                                             
     def exit_clicked(self):
